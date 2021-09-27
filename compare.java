15ab378883e56702af1bb5d29b5a58c68300fea4
pranav0297 <90312227+pranav0297@users.noreply.github.com>
2021-09-27 14:35:37 +0530

    compare.java

--- compare.java
+++ compare.java
@@ -1,46 +1,58 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
-public class compare implements Comparable<compare> {
-    private String name;
-    private int score;
-    private int age;
+class compare {
+    public static void main(String[] args) {
+        Comparator<user_5> com = new Comparator<user_5>() {
+            @Override
+            public int compare(user_5 o1, user_5 o2) {
+                int value = o1.score-o2.score;
+                if(value==0){
+                    return o1.name.compareTo(o2.name);
+                }
+                else
+                    return o1.score-o2.score;
+            }
+        };
+        ArrayList<user_5> Student = new ArrayList<user_5>();
+        user_5 s1 = new user_5("A", 75, 19);
+        user_5 s2 = new user_5("B", 80, 20);
+        user_5 s3 = new user_5("C", 75, 22);
+        user_5 s4 = new user_5("D", 76, 18);
 
-    public compare(String name, int score, int age) {
-        this.name = name;
-        this.score = score;
-        this.age = age;
+        Student.add(s1);
+        Student.add(s2);
+        Student.add(s3);
+        Student.add(s4);
+        System.out.println("Before Sorting");
+        System.out.println(Student+"\n");
+
+        System.out.println("After Sorting");
+        Collections.sort(Student,Collections.reverseOrder(com));
+        System.out.println(Student);
     }
 
-    @Override
-    public int compareTo(compare o) {
+    ArrayList<user_5> Student = new ArrayList<user_5>();
 
-        return this.score - o.score;
-    }
+    static public class user_5 {
+        String name;
+        int score;
+        int age;
 
-    @Override
-    public String toString() {
-        return "Name : " + name + " -> " + "Score : " + score + " Age" + " -> " + age + "\n";
-    }
-        public static void main (String[]args){
-            compare s1 = new compare("A", 50, 19);
-            compare s2 = new compare("B", 80, 20);
-            compare s3 = new compare("C", 75, 22);
-            compare s4 = new compare("D", 76, 18);
+        public user_5(String name, int score, int age) {
+            this.name = name;
+            this.score = score;
+            this.age = age;
+        }
 
-            List<compare> employees = new ArrayList<compare>();
-            employees.add(s1);
-            employees.add(s2);
-            employees.add(s3);
-            employees.add(s4);
-
-            System.out.println("Unsorted Data");
-            System.out.println(employees);
-
-
-            Collections.sort(employees,Collections.reverseOrder());
-
-            System.out.println("sorted Data");
-            System.out.println(employees);
+        @Override
+        public String toString() {
+            return "Name : " + name + " -> " + "Score : " + score + " Age" + " -> " + age + "\n";
         }
     }
+}
+
+
+
 
